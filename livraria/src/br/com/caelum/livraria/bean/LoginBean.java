6277e014233435e93549050629028375bb07ec27
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-05-03 13:19:23 -0300

    Login

---
+++ livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -0,0 +1,32 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.caelum.livraria.dao.UsuarioDAO;
+import br.com.caelum.livraria.modelo.Usuario;
+
+
+@ManagedBean
+@ViewScoped
+public class LoginBean {
+
+	private Usuario usuario = new Usuario();
+	
+	public String logar() {
+		System.out.println("Logar " + this.usuario.getEmail());
+		
+		boolean existe = new UsuarioDAO().existe(this.usuario);
+		
+		if(existe) {
+			return "livro?faces-redirect=true";
+		}
+		
+		return null;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+}
